b71b28c6f2e0676028db9d89f1ff5cb4c68a63c4
Rebecca Agustin <agustinr@mit.edu>
2019-05-15 06:25:51 -0400

    updated leap event listener

--- src/main.py
+++ src/main.py
@@ -10,9 +10,12 @@ sys.path.insert(0,"../lib")
 import Leap
 from Leap import SwipeGesture
 
-global POINTS, SWIPE, IMG
+global POINTS, SWIPE, CYCLE, BEAT_NUM, TIME_SIG
 POINTS = Queue.Queue()
 SWIPE = False
+CYCLE = False
+BEAT_NUM = 1
+TIME_SIG = 4
 
 class LeapEventListener(Leap.Listener):
     global POINTS, SWIPE
@@ -21,6 +24,8 @@ class LeapEventListener(Leap.Listener):
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
 
     def on_init(self, controller):
+        self.prev_position = None
+        self.prev_velocity = None
         print "Initialized"
 
     def on_connect(self, controller):
@@ -35,44 +40,36 @@ class LeapEventListener(Leap.Listener):
         print "Exited"
 
     def on_frame(self, controller):
-        # Get the most recent frame and report some basic information
         frame = controller.frame()
-
-        # print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
-        #       frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
-
-        # Get hands
+        timestamp = frame.timestamp
         for hand in frame.hands:
             if hand.is_right:
-                handType = "Right hand"
-                # print "  %s, id %d, position: %s" % (
-                #     handType, hand.id, hand.palm_position)
-                POINTS.put(hand.palm_position)
-                # print hand.palm_velocity
+                pos = hand.palm_position
+                vel = hand.palm_velocity
+                is_beat = False
+                if (BEAT_NUM%TIME_SIG==1 or BEAT_NUM%TIME_SIG==0):
+                    if (vel[1]/self.prev_velocity[1]<=0): 
+                        #abrupt stop/change in y direction -> recognize beat
+                        is_beat = True
+                else:
+                    if (vel[0]/self.prev_velocity[2]<=0):
+                        #abrupt stop/change in x direction -> recognize beat
+                        is_beat = True
+                POINTS.put(pos + [timestamp, is_beat])
+                self.prev_position = pos
+                self.prev_velocity = vel
 
-            # Get the hand's normal vector and direction
-            # normal = hand.palm_normal
-            # direction = hand.direction
-            # Calculate the hand's pitch, roll, and yaw angles
-            # print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-            #     direction.pitch * Leap.RAD_TO_DEG,
-            #     normal.roll * Leap.RAD_TO_DEG,
-            #     direction.yaw * Leap.RAD_TO_DEG)
-
-        # Get gestures
         for gesture in frame.gestures():
-            if gesture.type == Leap.Gesture.TYPE_SWIPE:
+            state = self.state_string(gesture.state)
+            command = gesture.type
+            if command == Leap.Gesture.TYPE_SWIPE:
                 swipe = SwipeGesture(gesture)
-                state = self.state_names[gesture.state]
                 if state == 'STATE_END':
-                    # print "Swipe Gesture Complete"
                     SWIPE = True
-                # print "  Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f" % (
-                #         gesture.id, self.state_names[gesture.state],
-                #         swipe.position, swipe.direction, swipe.speed)
-
-        if not (frame.hands.is_empty and frame.gestures().is_empty):
-            return
+            elif command == Leap.Gesture.TYPE_CIRCLE:
+                circle = CircleGesture(gesture)
+                if state == 'STATE_END':
+                    CYCLE = True
 
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
@@ -87,50 +84,22 @@ class LeapEventListener(Leap.Listener):
         if state == Leap.Gesture.STATE_INVALID:
             return "STATE_INVALID"
 
-class Tutorial:
-    global SWIPE
-    def __init__(self,root):
-        self.root = root
-        self.pg_num = 1
-
-        frame = Frame()
-        frame.pack()
-        self.page = Canvas(frame,width=1304,height=936)
-        self.page.grid(row=0,column=0)
-        self.pil_image = Image.open("../img/tutorial1.png")
-        width,height = self.pil_image.size
-        self.pil_image.resize((width,height),Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(self.pil_image,master=self.root)  
-        self.page.create_image(width/2,height/2,image=self.img,anchor="center")
-
-        self.next_page = Button()
-
-    def turn_page(self):
-        self.pg_num+=1
-        if self.pg_num == 4:
-            return True
-
-        file = "../img/tutorial"+str(self.pg_num)+".png"
-        self.pil_image = Image.open(file)
-        width,height = self.pil_image.size
-        self.pil_image.resize((width,height),Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(self.pil_image,master=self.root)  
-        self.page.create_image(width/2,height/2,image=self.img,anchor="center")
+        if state == Leap.Gesture.STATE_END:
+            return "STATE_END"
 
 class Multimodal_Metronome:
+    global TIME_SIG
     DIRECTIONS = ["LEFT","RIGHT","UP","DOWN"]
     def __init__(self,root):
         self.root = root
 
         self.time_types = ["4/4","3/4","2/4"]
-        self.references = {"":"../img/blank.png",0:"../img/blank.png",2:"../img/2-4.png",3:"../img/3-4.png",4:"../img/4-4.png"}
+        self.references = {"":"../img/blank.png",0:"../img/blank.png",2:"../img/2-4.png", \
+                            3:"../img/3-4.png",4:"../img/4-4.png"}
         self.met_bpm = 0
-        self.met_timesig = ""
         self.met_delay = 0
         self.met_count = 0
 
-        self.set_bpm = False
-        self.set_time = False
         self.metronome = False
         self.conducting = False
         self.shutdown = False
@@ -138,7 +107,6 @@ class Multimodal_Metronome:
         self.gui()
         self.metronome_thread = threading.Thread(target=self.metronome_handler)
         self.metronome_thread.start()
-        self.dummy_timer = 0
 
     def gui(self):
         frame = Frame()
@@ -189,58 +157,19 @@ class Multimodal_Metronome:
         self.quit_button.grid(row=4, column = 3,sticky="W")
         self.quit_button['font']=subtitle_font
 
-        self.met_reference = Canvas(frame,width=700,height=700,)
+        self.met_reference = Canvas(frame,width=700,height=700)
         self.met_reference.grid(row=5,column=0,columnspan=4,rowspan=10,sticky=fill,padx=300)
-        self.pil_image = Image.open("../img/4-4.png")
-        width,height = self.pil_image.size
-        self.pil_image.resize((width,height),Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(self.pil_image,master=self.root)  
-        self.met_reference.create_image(width/2,height/2,image=self.img,anchor="center")
-
-        self.trail_pts = []
-        self.update_times = []
-        self.cur_ind = 0
-        for i in range(60):
-            xo, yo = 350, 350
-            new_pt = self.met_reference.create_oval(xo, yo, xo+10, yo+10, fill='yellow')
-            self.trail_pts.append(new_pt)
-            self.update_times.append(time.time())
-
-    def update_trail(self,leap_data,cur_time):
-        xo,yo = 350,700
-        x,y = int(leap_data[0]*1.5), int(leap_data[1]*2)
-        xf, yf = max(0,min(xo+x,700)), max(0,min((yo-y),700))
-        cur_pt = self.trail_pts[self.cur_ind]
-        self.met_reference.coords(cur_pt,(xf,yf,xf+10,yf+10))
-        self.update_times[self.cur_ind]=cur_time
-        self.cur_ind += 1
-        self.cur_ind = self.cur_ind % len(self.trail_pts)
-        self.dummy_timer += 1
-
-    def tracking(self,leap_data):
-        if not self.conducting:
-            self.conducting = True
-            self.r_title['bg'] = 'green'
-        if self.dummy_timer > 300:
-            self.r1_display['text']=104
-        elif self.dummy_timer > 210:
-            self.r1_display['text']=106
-        elif self.dummy_timer > 120:
-            self.r2_display['text']='4/4'
-            self.r1_display['text']=98
+        self.update_time_sig()
 
     def quit(self):
         self.shutdown = True
 
+    def set_met_tempo(self,tempo):
+        self.met_bpm = tempo
+
     def start_met(self):
         if self.metronome:
             return #metronome is already on
-        try:
-            self.met_bpm = float(self.l1_display.get())
-            self.met_timesig = int(self.l2_display.get()[0])
-        except ValueError:
-            #voice feedback?
-            return
         self.metronome = True
         self.l_title['bg'] = 'green' 
 
@@ -262,13 +191,86 @@ class Multimodal_Metronome:
                 else:                   
                     Beep(440,300)
 
-                if self.met_count >=self.met_timesig:
+                if self.met_count >= TIME_SIG:
                     self.met_count = 0
 
                 time.sleep(self.met_delay-0.3)
 
+    def check_time_sig_select(self):
+        try:
+            return int(self.l2_display.get()[0])
+        except:
+            #voice feedback
+            return None
+
+    def check_tempo_select(self):
+        try:
+            return float(self.l1_display.get())
+        except:
+            # voice feedback
+            return None
+
+    def update_time_sig(self):
+        self.pil_image = Image.open(self.references[TIME_SIG])
+        width,height = self.pil_image.size
+        self.pil_image.resize((5*width,5*height),Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(self.pil_image,master=root)
+        self.met_reference.create_image(width/2,height/2,image=self.img,anchor="center")
+
+        self.trail_pts = []
+        self.update_times = []
+        self.cur_ind = 0
+        for i in range(60):
+            xo, yo = 350, 350
+            new_pt = self.met_reference.create_oval(xo, yo, xo+10, yo+10, fill='yellow')
+            self.trail_pts.append(new_pt)
+            self.update_times.append(time.time())
+
+    def update_trail(self,leap_data):
+        #leap_data = [x, y, z, timestamp, is_beat]
+        xo,yo = 350,700
+        x,y = int(leap_data[0]*1.5), int(leap_data[1]*2)
+        xf, yf = max(0,min(xo+x,700)), max(0,min((yo-y),700))
+        cur_pt = self.trail_pts[self.cur_ind]
+        self.met_reference.coords(cur_pt,(xf,yf,xf+10,yf+10))
+        self.update_times[self.cur_ind]=cur_time
+        self.cur_ind = (self.cur_ind+1) % len(self.trail_pts)
+
+    def tracking(self,leap_data):
+        #leap_data = [x, y, z, timestamp, is_beat]
+        return 0
+
+class Tutorial:
+    global SWIPE
+    def __init__(self,root):
+        self.root = root
+        self.pg_num = 1
+
+        frame = Frame()
+        frame.pack()
+        self.page = Canvas(frame,width=1304,height=936)
+        self.page.grid(row=0,column=0)
+        self.pil_image = Image.open("../img/tutorial1.png")
+        width,height = self.pil_image.size
+        self.pil_image.resize((width,height),Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(self.pil_image,master=self.root)  
+        self.page.create_image(width/2,height/2,image=self.img,anchor="center")
+
+        self.next_page = Button()
+
+    def turn_page(self):
+        self.pg_num+=1
+        if self.pg_num == 4:
+            return True
+
+        file = "../img/tutorial"+str(self.pg_num)+".png"
+        self.pil_image = Image.open(file)
+        width,height = self.pil_image.size
+        self.pil_image.resize((width,height),Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(self.pil_image,master=self.root)  
+        self.page.create_image(width/2,height/2,image=self.img,anchor="center")
+
 if __name__ == '__main__':
-    global SWIPE,POINTS
     root = Tk()
     root.configure(background='white')
 
@@ -282,28 +284,26 @@ if __name__ == '__main__':
     # tutorial.page.close()
 
     app = Multimodal_Metronome(root)
-    prev_timesig = ""
+    prev_time_sig = 4
+    prev_tempo = None
     prev_trail_update = time.time()
     empty_count = 0
-    while(1):
+    while(1): #tkinter updates
+        # check for quit button 
         if app.shutdown:
             break
-        app.met_timesig = int(app.l2_display.get()[0])
-        if app.met_timesig != prev_timesig:
-            app.pil_image = Image.open(app.references[app.met_timesig])
-            width,height = app.pil_image.size
-            app.pil_image.resize((5*width,5*height),Image.ANTIALIAS)
-            app.img = ImageTk.PhotoImage(app.pil_image,master=root)
-            app.met_reference.create_image(width/2,height/2,image=app.img,anchor="center")
-            prev_timesig = app.met_timesig
-            app.trail_pts=[]
-            app.update_times=[]
-            app.cur_ind=0
-            for i in range(60):
-                xo, yo = 350, 350
-                new_pt = app.met_reference.create_oval(xo, yo, xo+10, yo+10, fill='yellow')
-                app.trail_pts.append(new_pt)
-                app.update_times.append(time.time())
+
+        # check for user inputs to gui
+        time_sig_select = app.check_time_sig_select()
+        if time_sig_select and time_sig_select != prev_time_sig:
+            TIME_SIG = time_sig_select
+            prev_time_sig = TIME_SIG
+            app.update_time_sig()
+        tempo_select = app.check_tempo_select()
+        if tempo_select and tempo_select != prev_tempo:
+            app.set_met_tempo(tempo_select)
+
+        # if no motion inputs, stop conducting functions
         if POINTS.empty():
             if empty_count > 1500:
                 app.conducting = False
@@ -311,18 +311,21 @@ if __name__ == '__main__':
                 app.r1_display['text']='      '
                 app.r2_display['text']='      '
             empty_count +=1
+
+        # if new motion info, update visuals
         while not POINTS.empty():
             empty_count = 0
             data = POINTS.get()
             app.tracking(data)
             cur_time = time.time()
             if abs(cur_time - prev_trail_update) > 0.015:
-                app.update_trail(data,cur_time)
+                app.update_trail(data)
                 prev_trail_update = cur_time
         try:
             root.update()
         except:
             break
+
     controller.remove_listener(listener)
     root.destroy()
     
